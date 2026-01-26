a754c7afe70f9dfc30d7564c4290710f901607c4
pavan <sankojipavanchary123@gmail.com>
2026-01-26 23:21:01 +0530

    local

--- Day-4-state_lock/backend.tf
+++ Day-4-state_lock/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "state-lock-p"
     key = "terraform.tfstate"
-    use_lockfile = true #it enables S3-state locking
+    #use_lockfile = true #it enables S3-state locking
+    dynamodb_table ="pavan"
+    encrypt = true
     region = "eu-north-1"
   }
 }
