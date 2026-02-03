916aa4eb7a41a29314747f68b914c3aa84541294
pavan <sankojipavanchary123@gmail.com>
2026-02-03 10:04:15 +0530

    day-5 new changes

--- Day-5-custome-network/main.tf
+++ Day-5-custome-network/main.tf
@@ -1,17 +1,17 @@
-#vpc creation
+#creating vpc
 resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name="custom_vpc"
-  }
-}
-#subnets creation
-resource "aws_subnet" "name" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
-    availability_zone ="us-east-1a"
+    cidr_block = "10.0.0.0/16"
     tags = {
-      Name="subnet-1-public"
+      Name="custom-vpc"
+    }
+}
+# subnets creation
+resource "aws_subnet" "name" {
+    vpc_id =aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name="subnet-1-pub"
     }
 }
 resource "aws_subnet" "name2" {
@@ -22,14 +22,14 @@ resource "aws_subnet" "name2" {
       Name="subnet-prvt"
     }
 }
-# Internet gateway
+#IG creation
 resource "aws_internet_gateway" "name" {
     vpc_id = aws_vpc.name.id
     tags = {
       Name="IG"
     }
 }
-#Route table creation
+#Route gateway
 resource "aws_route_table" "name" {
     vpc_id = aws_vpc.name.id
     route {
@@ -40,90 +40,92 @@ resource "aws_route_table" "name" {
       Name="RT"
     }
 }
-# RT--subnet association
+# RT_subnet association
 resource "aws_route_table_association" "name" {
-    subnet_id = aws_subnet.name.id
     route_table_id = aws_route_table.name.id
-  
+    subnet_id = aws_subnet.name.id
 }
-# SG--cretion
+#sg-creation
 resource "aws_security_group" "name" {
     vpc_id = aws_vpc.name.id
-    name = "allow"     # it is mandatory
+    name = "allow"
     ingress {
-        description = "http"
+        description = "HTTP"
         from_port = 80
         to_port = 80
-        cidr_blocks = ["0.0.0.0/0"]
         protocol = "Tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
     ingress {
-        description = "https"
+        description = "HTTPS"
         from_port = 443
         to_port = 443
-        protocol = "Tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
     ingress {
         description = "SSH"
         from_port = 22
         to_port = 22
-        protocol = "Tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags= {
-        Name="new"
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
 }
-# creation of Ec2
+#Ec2 creation public
 resource "aws_instance" "name" {
     ami = "ami-0532be01f26a3de55"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.name.id
     vpc_security_group_ids = [ aws_security_group.name.id ]
-    associate_public_ip_address = true
-    tags= {
-        Name="custom_ec2"
+    tags = {
+      Name="ec2-pub"
     }
+    associate_public_ip_address = true   #it assign public ip
 }
-# creating prvt ec2
+# creating prvt Ec2
 resource "aws_instance" "name2" {
     ami = "ami-0532be01f26a3de55"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.name2.id
     vpc_security_group_ids = [ aws_security_group.name.id ]
-    tags= {
-        Name="prvt_ec2"
-    } 
+    tags = {
+      Name="ec2-prvt"
+    }
+  
 }
-#elastic ip
+#to create NAT frst we need to create "elastic_ip"
+#creating E_ip
 resource "aws_eip" "name" {
     domain = "vpc"
 }
-# creating NAT gateway
+# NAT gateway
 resource "aws_nat_gateway" "name" {
+    subnet_id = aws_subnet.name2.id
     allocation_id = aws_eip.name.id
-    subnet_id =aws_subnet.name.id
+    tags = {
+      Name="NAT"
+    }
 }
-#create rt
+# craeting rt
 resource "aws_route_table" "name2" {
     vpc_id = aws_vpc.name.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.name.id    
+        gateway_id = aws_nat_gateway.name.id
     }
     tags = {
-      Name="pr-rt"
+      Name="pvt-rt"
     }
 }
-# nat- subnet association
+#RT subnet association
 resource "aws_route_table_association" "name2" {
     subnet_id = aws_subnet.name2.id
     route_table_id = aws_route_table.name2.id
+  
 }
